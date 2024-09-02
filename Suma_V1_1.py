6c09aa258880d39a46b420b6c529d13d5a9cbe4d
Riptolee <72810034@continental.edu.pe>
2024-09-02 18:42:39 -0500

    se mejoro Suma V1_1

--- Suma_V1_1.py
+++ Suma_V1_1.py
@@ -1,12 +1,15 @@
 def ingresoNumeros():
-    numero1= float(input("Ingrese sumando uno: "))
-    numero2= float(input("Ingrese sumando dos: "))
-    return numero1, numero2
+    # Solicita al usuario ingresar dos números
+    numero01 = float(input("Ingrese sumando uno: "))
+    numero02 = float(input("Ingrese sumando dos: "))
+    return numero01, numero02
 
-
-def suma(numero1, numero2):
-    return numero1 + numero2
+def suma(numero01, numero02):
+    # Suma los dos números y retorna el resultado
+    return numero01 + numero02
 
 if __name__ == "__main__":
+    # Llama a ingresoNumeros para obtener los dos números
     num1, num2 = ingresoNumeros()
+    # Imprime el resultado de la suma
     print(f"{num1} + {num2} = {suma(num1, num2)}")
